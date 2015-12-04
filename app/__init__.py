908fc6d9064bf902a537540fe0ab65165b1cca9d
Kartik Chopra <Kartik@Kartiks-MBP.home>
2015-12-04 00:45:28 -0500

    first commit for backendDB

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import mysql.connector
+
 from flask import Flask
 
 #importing Flask (class that holds the entire app)
@@ -6,3 +8,27 @@ from flask import Flask
 
 app = Flask(__name__)
 from app import views
+
+
+connection = mysql.connector.connect(user='kc650', password='rucharity',
+                              host='rutgerscharity.cjsliricy0ds.us-east-1.rds.amazonaws.com',
+                              database='MainCharityDB')
+
+cursor = connection.cursor()
+
+operation = 'SELECT count(name) from Givers'
+for result in cursor.execute(operation, multi=True):
+  if result.with_rows:
+    print("Rows produced by statement")
+    print(result.fetchall())
+  else:
+    print("Number of rows affected by statement '{}': {}".format(
+      result.statement, result.rowcount))
+
+
+cursor.execute(operation)
+#Command executed!
+
+
+
+connection.close()
