25c8a3304fb09db87cbb691334b7f79e284665d0
Kartik Chopra <Kartik@nbp-135-139.nbp.ruw.rutgers.edu>
2015-12-05 18:00:10 -0500

    Submit pushes to DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from flask import request
 from flask import Flask
+from flask import render_template, json
 
 #importing Flask (class that holds the entire app)
 
@@ -9,11 +10,12 @@ from flask import Flask
 app = Flask(__name__)
 from app import views
 
-
+global connection
 connection = mysql.connector.connect(user='kc650', password='rucharity',
                               host='rutgerscharity.cjsliricy0ds.us-east-1.rds.amazonaws.com',
                               database='MainCharityDB')
 
+global cursor;
 cursor = connection.cursor()
 
 operation = 'SELECT count(name) from Givers'
@@ -27,8 +29,47 @@ for result in cursor.execute(operation, multi=True):
 
 
 cursor.execute(operation)
+
+@app.route('/signUp', methods=["GET"])
+def signUp():
+    global connection
+    connection = mysql.connector.connect(user='kc650', password='rucharity',
+                                  host='rutgerscharity.cjsliricy0ds.us-east-1.rds.amazonaws.com',
+                                  database='MainCharityDB')
+    global cursor;
+    cursor = connection.cursor()
+
+
+    #read values of Giving Form
+    _name = request.args.get("UserName")
+    _RUID = request.args.get("RUID")
+    _MEALPLAN = request.args.get("MealPlan")
+    _SwipesLeft = request.args.get("SwipesLeft")
+    _MAJOR = request.args.get("Major")
+    _CAMPUS = request.args.get("Campus")
+
+    #read values for Recieveing form
+    _Rname = request.args.get("rUserName")
+    _rRUID = request.args.get("r_RUID")
+    _ANNUALINCOME = request.args.get("AnnualIncome")
+    _SWIPESRECIEVED = 0
+    _COMMUTER = request.args.get("Commuter")
+    _MAJOR = request.args.get("r_Major")
+
+    #if _name and _RUID and _MEALPLAN and _SwipesLeft and _MAJOR and _CAMPUS: ('test',1451001509, 150,0,'Finance','Busch')
+
+    query_giver = """INSERT INTO Givers (Name,RUID,MEALPLAN,SWIPES_Left,MAJORS,CAMPUS)
+                VALUES (%s,%s,%s,%s,%s,%s); """
+
+    cursor.execute(query_giver, (_name, _RUID, _MEALPLAN, _SwipesLeft, _MAJOR, _CAMPUS))
+    connection.commit()
+
+    query_reciever = """INSERT INTO Recievers (Name,RUID,Annual_Income,Swipes_Recieved,Commuter,Major)
+                VALUES (%s,%s,%s,%s,%s,%s); """
+
+    cursor.execute(query_reciever, (_Rname, _rRUID, _ANNUALINCOME, _SWIPESRECIEVED, _COMMUTER, _MAJOR))
+    connection.commit()
+    return render_template("index.html")
+
+
 print ("Command executed!");
-
-
-
-connection.close()
